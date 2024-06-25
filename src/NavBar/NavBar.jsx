ce01bb5d546f8dd6688c7b7827f679638e97a4b4
Sai Sidharthan <saisidharthan@gamil.com>
2024-06-25 12:47:36 +0530

    footer updated

---
+++ src/NavBar/NavBar.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from '@mui/material';
+import Logo from '../Assets/LOGO.ico';
+import './NavBar.css';
+
+const NavBar = () => {
+  const [menu, setMenu] = useState("");
+
+  return (
+    <div className="navbar">
+      <div className="nav-logo">
+        <img src={Logo} alt='img' />
+      </div>
+      <ul className="nav-menu">
+        <li onClick={() => { setMenu("Home") }}>
+          <Button
+            component={Link}
+            to="/"
+            id='but1'
+            className={menu === 'Home' ? 'active' : ''}
+          >
+            Home
+          </Button>
+          {menu === 'Home' ? <hr /> : null}
+        </li>
+        <li onClick={() => { setMenu("About Me") }}>
+          <Button
+            component={Link}
+            to="/Aboutme"
+            id='but2'
+            className={menu === 'About Me' ? 'active' : ''}
+          >
+            About Me
+          </Button>
+          {menu === 'About Me' ? <hr /> : null}
+        </li>
+        <li onClick={() => { setMenu("Contact Me") }}>
+          <Button
+            component={Link}
+            to="/Contactme"
+            id='but3'
+            className={menu === 'Contact Me' ? 'active' : ''}
+          >
+            Contact Me
+          </Button>
+          {menu === 'Contact Me' ? <hr /> : null}
+        </li>
+        <li onClick={() => { setMenu("Experience") }}>
+          <Button
+            component={Link}
+            to="/Experience"
+            id='but4'
+            className={menu === 'Experience' ? 'active' : ''}
+          >
+            Experience
+          </Button>
+          {menu === 'Experience' ? <hr /> : null}
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default NavBar;
+
---
+++ src/Pages/ContactMe.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from '../NavBar/NavBar'
+
+const ContactMe = () => {
+  return (
+    <div>
+        <NavBar/>
+    </div>
+  )
+}
+
+export default ContactMe
